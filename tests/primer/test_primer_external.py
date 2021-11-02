dc77b1fa3a5dfa782787eadc47dfb68be2ac4d49
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-02 16:03:49 +0100

    Update primer tests

--- tests/primer/test_primer_external.py
+++ tests/primer/test_primer_external.py
@@ -15,22 +15,25 @@ from tests.primer.primer_external_packages import (
 class TestPrimer:
     @staticmethod
     @pytest.mark.primer
-    @pytest.mark.parametrize(("package"), PACKAGES_TO_LINT.values())
+    @pytest.mark.parametrize(("package"), PACKAGES_TO_LINT)
     def test_primer_external_packages_no_crash(package: PackageToLint):
         """Runs pylint over external packages to check for crashes"""
-        clone_primer_packages(package)
+        package_data = PACKAGES_TO_LINT[package]
+
+        # Clone the packages repository
+        clone_primer_packages(package_data)
 
         try:
-            # We only check for crashes
-            # We suppose that errors in a big lib come from a pylint false positive
             subprocess.run(
                 ["pylint"]
                 + [
-                    f"{PRIMER_DIRECTORY}{package.clone_directory}/{i}"
-                    for i in package.directories.split(" ")
+                    f"{PRIMER_DIRECTORY}{package_data.clone_directory}/{i}"
+                    for i in package_data.directories.split(" ")
                 ]
                 + ["--errors-only", "--rcfile=./pylintrc"],
                 check=True,
             )
         except subprocess.CalledProcessError as ex:
+            # We only check for crashes (error code 32)
+            # We suppose that errors in a big lib come from a pylint false positive
             assert ex.returncode != 32
