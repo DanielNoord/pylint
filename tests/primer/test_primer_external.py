7dea88732ce58f5dd69b8d9bc9bd36ca93b71d44
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-02 15:09:10 +0100

    Update primer

--- tests/primer/test_primer_external.py
+++ tests/primer/test_primer_external.py
@@ -1,32 +1,36 @@
 # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 # For details: https://github.com/PyCQA/pylint/blob/main/LICENSE
 
-import os
 import subprocess
 
 import pytest
 from tests.primer.primer_clone_external import clone_primer_packages
-from tests.primer.primer_external_packages import PACKAGES_TO_LINT, PRIMER_DIRECTORY
+from tests.primer.primer_external_packages import (
+    PACKAGES_TO_LINT,
+    PRIMER_DIRECTORY,
+    PackageToLint,
+)
 
 
 class TestPrimer:
     @staticmethod
     @pytest.mark.primer
-    @pytest.mark.parametrize(("package"), PACKAGES_TO_LINT)
-    def test_primer_external_packages_no_crash(package: str):
+    @pytest.mark.parametrize(("package"), PACKAGES_TO_LINT.items())
+    def test_primer_external_packages_no_crash(package: PackageToLint):
         """Runs pylint over external packages to check for crashes"""
-        package_data = PACKAGES_TO_LINT[package]
+        clone_primer_packages(package)
 
-        clone_primer_packages(package_data)
-
-        os.chdir(f"{PRIMER_DIRECTORY}{package_data.clone_directory}")
         try:
-            # We only check for crashs
+            # We only check for crashes
             # We suppose that errors in a big lib come from a pylint false positive
             subprocess.run(
-                ["pylint"] + package_data.directories.split(" ") + ["--errors-only"],
+                ["pylint"]
+                + [
+                    f"{PRIMER_DIRECTORY}{package.clone_directory}/{i}"
+                    for i in package.directories.split(" ")
+                ]
+                + ["--errors-only", "--rcfile=./pylintrc"],
                 check=True,
             )
-        except SystemExit as ex:
-            assert ex.code != 32
-            return
+        except subprocess.CalledProcessError as ex:
+            assert ex.returncode != 32
