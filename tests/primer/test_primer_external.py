66b3609716107f552a5dcb43764ed4785ad4b2c1
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-02 11:37:36 +0100

    Fix error code

--- tests/primer/test_primer_external.py
+++ tests/primer/test_primer_external.py
@@ -9,19 +9,27 @@ from tests.primer.primer_clone_external import clone_primer_packages
 from tests.primer.primer_external_packages import PACKAGES_TO_LINT, PRIMER_DIRECTORY
 
 
-@pytest.mark.primer
-def test_primer_external_packages():
-    """Downloads external packages and runs pylint over them to check for crashes"""
-    clone_primer_packages()
-    for data in PACKAGES_TO_LINT.values():
-        os.chdir(f"{PRIMER_DIRECTORY}{data.clone_directory}")
+class TestPrimer:
+    @staticmethod
+    def setup_class():
+        """Sets up the primer tests by downloading external packages to run pylint over"""
+        clone_primer_packages()
+
+    @staticmethod
+    @pytest.mark.primer
+    @pytest.mark.parametrize(("package"), PACKAGES_TO_LINT)
+    def test_primer_external_packages_no_crash(package: str):
+        """Runs pylint over external packages to check for crashes"""
+        package_data = PACKAGES_TO_LINT[package]
+
+        os.chdir(f"{PRIMER_DIRECTORY}{package_data.clone_directory}")
         try:
-            # We only check for crash and errors as warning are not stable
+            # We only check for crashs
             # We suppose that errors in a big lib come from a pylint false positive
             subprocess.run(
-                ["pylint"] + data.directories.split(" ") + ["--errors-only"],
+                ["pylint"] + package_data.directories.split(" ") + ["--errors-only"],
                 check=True,
             )
         except SystemExit as ex:
-            assert ex.code == 0
+            assert ex.code != 32
             return
